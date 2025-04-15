dd8877e91c478ee29b81a2d033855bd1199d29e2
EMC-MSK-Officemag <153815004+EMC-MSK-Officemag@users.noreply.github.com>
2025-04-15 17:57:12 +0300

    15042025

--- src/main.js
+++ src/main.js
@@ -1,10 +1,35 @@
 const listOfTopics = document.getElementById("listOfTopics");
+const topics = document.getElementById("topics");
+const content = document.getElementById("content");
 
-topicsDescription.map((el, index) => {
-    const item = document.createElement("li");
-    item.innerHTML = `Урок ${index + 1}`;
-    item.title = el.key;
-    listOfTopics.append(item);
+topics.addEventListener("click", (event) => {
+    topics.style.background = "burlywood";
+    content.style.background = "antiquewhite";   
+
+    for (let index = 0; index < listOfTopics.children.length; index++) {
+        listOfTopics.children[index].onclick = null;
+        listOfTopics.children[index].id = "listOfTopicsItem" + index;
+        // console.log(listOfTopics.children[index].disabled = "true")
+        
+    }
+
 })
 
-console.log(listOfTopics);
+content.addEventListener("click", (event) => {
+    content.style.background = "burlywood";
+    topics.style.background = "antiquewhite";
+})
+// Размещаем пункты из содержания в левом меню
+topicsDescription.map((el, index) => {
+  const item = document.createElement("li");
+  item.className = "listOfTopicsItem";
+  item.innerHTML = `Урок ${index + 1}`;  
+  item.title = Object.keys(el);
+  listOfTopics.append(item);
+});
+
+listOfTopics.addEventListener("click", (event) => {
+    console.log(event.target);
+
+})
+
