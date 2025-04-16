1cc11ef545f5902e8704ac49efaf0a3b35d2e1d7
EMC-MSK-Officemag <153815004+EMC-MSK-Officemag@users.noreply.github.com>
2025-04-16 16:52:08 +0300

    16042025 v1

--- src/main.js
+++ src/main.js
@@ -1,35 +1,76 @@
 const listOfTopics = document.getElementById("listOfTopics");
 const topics = document.getElementById("topics");
 const content = document.getElementById("content");
+const descriptionOfChoiseTopic = document.getElementById("descriptionOfChoiseTopic");
+const placeForProjects = document.getElementById("placeForProjects");
+const listOfTopicsItem = document.getElementsByClassName("listOfTopicsItem");
 
-topics.addEventListener("click", (event) => {
+
+// Параметры раздела с содержанием
+topics.addEventListener("click", (event) => {  
+    placeForProjects.style.display = "none";
+    descriptionOfChoiseTopic.style.display = "block";
     topics.style.background = "burlywood";
-    content.style.background = "antiquewhite";   
-
-    for (let index = 0; index < listOfTopics.children.length; index++) {
-        listOfTopics.children[index].onclick = null;
-        listOfTopics.children[index].id = "listOfTopicsItem" + index;
-        // console.log(listOfTopics.children[index].disabled = "true")
-        
+    content.style.background = "antiquewhite"; 
+    for (let index = 0; index < listOfTopicsItem.length; index++) {
+        listOfTopicsItem[index].style.color = "saddlebrown";   
+        listOfTopicsItem[index].style.textDecoration = "none";     
     }
+    descriptionOfChoiseTopic.innerHTML = "Выберите урок, чтобы ознакомиться с его содержанием.";
+});
 
-})
-
-content.addEventListener("click", (event) => {
-    content.style.background = "burlywood";
-    topics.style.background = "antiquewhite";
-})
 // Размещаем пункты из содержания в левом меню
 topicsDescription.map((el, index) => {
   const item = document.createElement("li");
   item.className = "listOfTopicsItem";
+  item.id = index + "_listOfTopicsItem";
   item.innerHTML = `Урок ${index + 1}`;  
   item.title = Object.keys(el);
   listOfTopics.append(item);
 });
 
-listOfTopics.addEventListener("click", (event) => {
-    console.log(event.target);
+// Начальные настройки разделов
+placeForProjects.style.display = "none";
+for (let index = 0; index < listOfTopicsItem.length; index++) {
+    listOfTopicsItem[index].style.color = "saddlebrown";
+    listOfTopicsItem[index].style.textDecoration = "none";    
+    console.log(listOfTopicsItem.length);    
+}
+
+// Клик на "Проекты учеников"
+content.addEventListener("click", (event) => {
+    placeForProjects.innerHTML = "";
+    placeForProjects.style.display = "block";
+    descriptionOfChoiseTopic.style.display = "none";
+    content.style.background = "burlywood";
+    topics.style.background = "antiquewhite";
+    for (let index = 0; index < listOfTopicsItem.length; index++) {
+        listOfTopicsItem[index].style.color = "antiquewhite";   
+        listOfTopicsItem[index].style.textDecoration = "none";     
+    }
+    // Выводим проекты учеников на главную страницу
+    const projectItem = document.createElement("li");
+    const projectLink = document.createElement("a");
+    projectLink.href = "./studentProjects/001Andrey/index.html";
+    projectLink.target = "blanck";
+    projectLink.innerHTML = "001Andrey";
+    projectItem.append(projectLink);
+    placeForProjects.append(projectItem);
 
 })
+
+// Клик на тему из "Содержания курса"
+listOfTopics.addEventListener("click", (event) => {
+    topics.style.background = "burlywood";
+    content.style.background = "antiquewhite"; 
+    for (let index = 0; index < listOfTopicsItem.length; index++) {
+        listOfTopicsItem[index].style.color = "saddlebrown";
+        listOfTopicsItem[index].style.textDecoration = "none";        
+    }
+    placeForProjects.style.display = "none";
+    descriptionOfChoiseTopic.style.display = "block";        
+    descriptionOfChoiseTopic.innerHTML =`<b>${event.target.innerHTML}. ${event.target.title}</b><br><br> ${Object.values(topicsDescription[event.target.id.split("_")[0]])}`;
+    document.getElementById(event.target.id).style.textDecoration = "underline";
+})
+
 
