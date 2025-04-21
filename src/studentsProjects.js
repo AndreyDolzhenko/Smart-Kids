00fa7f044573e9d4d70c47c6ddad21f09b73f0b1
EMC-MSK-Officemag <153815004+EMC-MSK-Officemag@users.noreply.github.com>
2025-04-21 18:16:37 +0300

    21042025

--- src/studentsProjects.js
+++ src/studentsProjects.js
@@ -1,9 +1,11 @@
 const nameOfProject = ["Андрей Долженко", "Lena", "Ivan", "Nikita", "Maria", "Vika", ];
 
-const date = `${new Date().getDate()}.${new Date().getMonth() + 1}.${new Date().getFullYear()}`;
 
-const startName = ["Andrey", "Lena", "Ivan", "Nikita", "Maria", "Vika", ];
 
-const currentName = startName.map(el => el + ". " + date);
+// const date = `${new Date().getDate()}.${new Date().getMonth() + 1}.${new Date().getFullYear()}`;
 
-console.log(currentName);
+// const startName = ["Andrey", "Lena", "Ivan", "Nikita", "Maria", "Vika", ];
+
+// const currentName = startName.map(el => el + ". " + date);
+
+// console.log(currentName);
